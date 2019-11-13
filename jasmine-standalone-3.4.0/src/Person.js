2a5982884b2109e30e246085600ee654d83cd751
lwandile <lwandile.mk3@gmail.com>
2019-11-13 14:10:47 +0200

    modified

--- jasmine-standalone-3.4.0/src/Person.js
+++ jasmine-standalone-3.4.0/src/Person.js
@@ -7,7 +7,13 @@ class Person{
     }
     hello(){
         let lastIntest= this.interests.pop();
+        
        return `Hello, my name is ${this.name} and I am ${this.age} years old. My interests are being a ${this.interests} and `+ lastIntest;
     }
 }
+
+let person = new Person('lwandile', 12, 'male', ['being a hardarse','agile', 'ssd hard drives'])
+let me = person.hello();
+// console.log(me);
+
 
